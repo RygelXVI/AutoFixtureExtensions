13f94b26a5590fbd288337960b7029fa47c72156
Andrew Morshead <andrew.cm74@outlook.com>
2025-12-02 17:08:37 +0100

    added extension methods to allow registration of httpclients using a custom delegating handler, added a simple delegating handler to match on routes, added tests

--- src/AutoFixture.Extensions.Http/AutoFixtureExtensions.cs
+++ src/AutoFixture.Extensions.Http/AutoFixtureExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using AutoFixture.Extensions.Http.Helpers;
 using AutoFixture.Kernel;
 using RichardSzalay.MockHttp;
+using System.Net;
 
 namespace AutoFixture.Extensions.Http;
 
@@ -29,6 +30,22 @@ public static class AutoFixtureExtensions
     }
 
     /// <summary>
+    /// Configures the fixture to use an HTTP client with the specified name and delegating handler.
+    /// </summary>
+    /// <param name="httpClientName">The name to associate with the created HTTP client.</param>
+    /// <param name="delegatingHandler">The delegating handler to use for processing HTTP request and response messages. Cannot be null.</param>
+    /// <returns>An updated fixture instance configured with the specified HTTP client and handler.</returns>
+    public static IFixture WithHttpClientFactory(this IFixture fixture, string httpClientName, DelegatingHandler delegatingHandler)
+    {
+        var httpClient = new Dictionary<string, HttpClient>()
+        {
+            { httpClientName, CreateHttpClient(delegatingHandler) }
+        };
+
+        return WithHttpClientFactory(fixture, httpClient);
+    }
+
+    /// <summary>
     /// Allows registration of multiple named MockHttpMessageHandlers which the system under test can access via an IHttpClientFactory
     /// Uses a simple factory implementation for testing.
     /// </summary>
@@ -39,6 +56,23 @@ public static class AutoFixtureExtensions
     public static IFixture WithHttpClientFactory(this IFixture fixture, params (string httpClientName, MockHttpMessageHandler mockHttpMessageHandler)[] httpClients)
     {
         var clients = httpClients.ToDictionary(x => x.httpClientName, x => x.mockHttpMessageHandler.ToHttpClient());
+
+        return WithHttpClientFactory(fixture, clients);
+    }
+
+    /// <summary>
+    /// Configures the fixture to use custom HTTP clients created from the specified names and delegating handlers.
+    /// </summary>
+    /// <remarks>Each tuple in the array results in a new HttpClient instance associated with the
+    /// given name and handler. This method is useful for testing scenarios that require custom HTTP client
+    /// configurations.</remarks>
+    /// <param name="httpClients">An array of tuples, each containing the name of the HTTP client and the delegating handler to be used for
+    /// that client. The client name must not be null or empty. The delegating handler is used to configure the HTTP
+    /// client's request pipeline.</param>
+    /// <returns>An updated fixture instance configured with the specified HTTP clients.</returns>
+    public static IFixture WithHttpClientFactory(this IFixture fixture, params (string httpClientName, DelegatingHandler delegatingHandler)[] httpClients)
+    {
+        var clients = httpClients.ToDictionary(x => x.httpClientName, x => CreateHttpClient(x.delegatingHandler));
 
         return WithHttpClientFactory(fixture, clients);
     }
@@ -83,5 +117,35 @@ public static class AutoFixtureExtensions
         var httpClient = mockHttpMessageHandler.ToHttpClient();
         fixture.Register(() => httpClient);
         return fixture;
+    }
+
+    public static IFixture WithHttpClient(this IFixture fixture, DelegatingHandler delegatingHandler)
+    {
+        var httpClient = CreateHttpClient(delegatingHandler);
+        fixture.Register(() => httpClient);
+        return fixture;
+    }
+
+    public static IFixture WithHttpClient(this IFixture fixture, params (string route, HttpStatusCode responseStatus)[] routesAndResponses)
+    {
+        var testHandler = new SimpleRouteHandler();
+
+        foreach (var (route, responseStatus) in routesAndResponses)
+        {
+            testHandler.WithRoute(route, responseStatus);
+        }
+
+        var httpClient = CreateHttpClient(testHandler);
+        fixture.Register(() => httpClient);
+        return fixture;
+    }
+
+    private static HttpClient CreateHttpClient(DelegatingHandler delegatingHandler)
+    {
+        var result = new HttpClient(delegatingHandler)
+        {
+            BaseAddress = new Uri("http://local/")
+        };
+        return result;
     }
 }
---
+++ tests/AutoFixture.Extensions.Http.Specification/SimpleRouteHandlerSpecification.cs
@@ -0,0 +1,48 @@
+﻿using AutoFixture.Extensions.Http.Helpers;
+using System.Net;
+
+namespace AutoFixture.Extensions.Http.Specification;
+
+public class SimpleRouteHandlerSpecification
+{
+    [Fact]
+    public void Can_register_simple_route_handler()
+    {
+        var handler = new SimpleRouteHandler();
+        var fixture = new Fixture().WithHttpClient(handler);
+
+        var client = fixture.Create<HttpClient>();
+
+        Assert.NotNull(client);
+    }
+
+    [Fact]
+    public void Can_register_simple_route_handler_with_factory()
+    {
+        var clientName = nameof(SimpleRouteHandler);
+
+        var handler = new SimpleRouteHandler();
+        var fixture = new Fixture().WithHttpClientFactory(clientName, handler);
+
+        var factory = fixture.Freeze<IHttpClientFactory>();
+
+        Assert.NotNull(factory);
+
+        var client = factory.CreateClient(clientName);
+
+        Assert.NotNull(client);
+    }
+
+    [Fact]
+    public async Task Can_register_route_and_response_for_simple_route_handler_directly()
+    {
+        var fixture = new Fixture().WithHttpClient(("/all", HttpStatusCode.UnavailableForLegalReasons));
+
+        var client = fixture.Freeze<HttpClient>();
+
+        var request = new HttpRequestMessage(HttpMethod.Get, "http://local/all");
+        var response = await client.SendAsync(request, CancellationToken.None);
+
+        Assert.Equal(HttpStatusCode.UnavailableForLegalReasons, response.StatusCode);
+    }
+}
